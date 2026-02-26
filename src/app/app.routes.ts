2781fcceac2502d7ddc180d78005406fc01c8cd4
Elias Arango Salvador <eliasjuan.as.14@gmail.com>
2026-02-26 00:02:59 -0500

    feat(auth): estructura inicial del login y limpieza de app component - 26/02/2026

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,15 +5,33 @@ import { Routes } from '@angular/router';
  * Arquitectura modular con lazy loading
  */
 export const routes: Routes = [
+  // 1. Módulo Público (Login)
+  {
+    path: 'auth',
+    loadChildren: () => 
+      import('./modules/auth/auth-module').then(m => m.AuthModule)
+  },
+  
+  // 2. Módulos Privados / Protegidos
   {
     path: 'comercial',
     loadChildren: () => 
       import('./modules/area-estrategia-desarrollo-comercial/comercial.routes').then(m => m.COMERCIAL_ROUTES)
+    // TODO: Más adelante agregaremos un "canActivate: [AuthGuard]" aquí
   },
+  
   // TODO: Agregar más módulos (académico, financiero, rrhh, etc.)
+
+  // 3. Redirección por defecto al Login
   {
     path: '',
-    redirectTo: 'comercial',
+    redirectTo: 'auth', // O 'auth/login' dependiendo de cómo configures las rutas hijas del auth
     pathMatch: 'full'
+  },
+
+  // 4. Ruta comodín (Catch-all) por si escriben una URL que no existe
+  {
+    path: '**',
+    redirectTo: 'auth'
   }
 ];
